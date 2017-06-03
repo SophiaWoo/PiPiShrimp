2214c90e9f988a45637fe1a636e3db6f842efffc
kungcn <kcnnow@gmail.com>
2017-06-03 11:02:16 +0800

    add config in main.js

---
+++ src/components/signin.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+	  <el-button type="primary">inex</el-button>
+	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'index',
+	  data () {
+	    return {
+	    }
+	  }
+	}
+</script>
+
+<style>
+	
+</style>
