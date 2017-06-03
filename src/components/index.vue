b30cdb10910cf4c987973606baf4f30af56b73a8
SophiaWoo <w.u.s.i@163.com>
2017-06-03 19:38:30 +0800

    extract the movie-list part for reusing

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,31 +1,22 @@
 <template>
-  <div>
+  <div class="index-page">
     <div calss="recommend-movie">
       <el-carousel indicator-position="outside">
         <el-carousel-item v-for="item in recommendMovies" :key="item">
-          <img v-bind:src="item.imgUrl"/>
+          <img v-bind:src="item.bannerUrl"/>
         </el-carousel-item>
       </el-carousel>
     </div>
     <div>
       <el-tabs v-model="activeTab" @tab-click="changeTab">
         <el-tab-pane label="正在热映" name="hot-online">
-          <el-row :gutter="10" :type="flex" justify="start">
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-          </el-row>
+          <movies :movieList="hotOnline"></movies>
         </el-tab-pane>
-        <el-tab-pane label="即将上映" name="comming-soon">
-          
+        <el-tab-pane label="即将上映" name="coming-soon">
+          <movies :movieList="comingSoon"></movies>
         </el-tab-pane>
         <el-tab-pane label="热门电影" name="top-rate">
-          
+          <movies :movieList="topRate"></movies>
         </el-tab-pane>
       </el-tabs>
     </div>  
@@ -33,18 +24,29 @@
 </template>
 
 <script>
+import movies from './movies'
 export default {
   name: 'index',
   data () {
     return {
+      database: "http://localhost:3003",
       recommendMovies: [],
+      hotOnline: [],
+      comingSoon: [],
+      topRate: [],
       activeTab: 'hot-online'
     }
   },
   methods: {
     getMovies() {
-      this.$http.get("http://localhost:3003/movies").then(response => {
+      this.$http.get(this.database+"/allMv").then(response => {
         this.recommendMovies = response.data
+        this.hotOnline = response.data
+      }, response => {
+      });
+      this.$http.get(this.database+"/onShowMv").then(response => {
+        this.comingSoon = response.data
+        this.topRate = response.data
       }, response => {
       });
     },
@@ -54,11 +56,18 @@ export default {
   },
   mounted: function() {
     this.getMovies()
+  },
+  components: {
+    movies
   }
 }
+
 </script>
 
 <style scoped>
+  .index-page {
+    min-width: 960px;
+  }
   /*swiper*/
   .el-carousel__item img {
     width: 100%;
@@ -73,17 +82,5 @@ export default {
   /*tab*/
   .el-tab-pane {
     padding: 0 20px;
-  }
-  /*tab-contain*/
-  .el-col {
-    margin-bottom: 20px;
-    border-radius: 4px;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 320px;
   }
 </style>
---
+++ src/components/movies.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <el-row :gutter="20" type="flex" justify="start">
+            <el-col :span="6" v-for="item in movieList" :key="item">
+                <div class="grid-content bg-purple">
+                <div class="cover"><img v-bind:src="item.imgUrl"/></div>
+                <div class="info">
+                    <el-rate v-model="item.score" :max="5" disabled show-text text-color="#ff9900" text-template="{value}"></el-rate>
+                    <span class="movie-name name">{{item.moviename}}</span>
+                    <span class="name">{{item.director}}</span>
+                </div>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'movies',
+  props: ['movieList']
+}
+</script>
+
+<style scoped>
+  .el-col {
+    margin-bottom: 20px;
+    border-radius: 4px;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .grid-content {
+    border-radius: 4px;
+    height: 320px;
+  }
+  /*movie-list*/
+   .cover {
+    width:100%;
+    height: 80%;
+  }
+  .cover img{
+    width:100%;
+    height: 100%;
+  }
+  .info {
+    height: 20%;
+    width: 100%;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+  }
+  .info .el-rate {
+    position: absolute;
+    right: -15px;
+    top: 5px;
+    transform: scale(0.7);
+  }
+  .info .name {
+    margin-left: 10px;
+    font-size: 14px;
+  }
+  .info .movie-name {
+    font-weight: bold;
+  }
+</style>
